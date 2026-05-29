42e82b1c25cb11229ae3c34e7b807a8e7e80410d
Sergey Miryanov <sergey.miryanov@gmail.com>
2026-05-29 23:59:26 +0500

    Debug...

--- tests/monitoring/test_run_cmd.py
+++ tests/monitoring/test_run_cmd.py
@@ -82,24 +82,33 @@ def run_script(script_file: Path, *script_args: str, gc_args: list[str] | None =
 
 def run_module(module_name: str, *script_args: str, gc_args: list[str] | None = None) -> subprocess.CompletedProcess[str]:
     gc_opts = gc_args or []
-    proc = subprocess.run(
-        [
-            sys.executable,
-            "-u",
-            "-m",
-            "gc_monitor",
-            "run",
-            *gc_opts,
-            "-m",
-            str(module_name),
-        ] + list(script_args),
-        # capture_output=True,
-        stdout=subprocess.PIPE,
-        stderr=subprocess.STDOUT,
-        text=True,
-        timeout=30,
-    )
-    return proc
+    try:
+        proc = subprocess.run(
+            [
+                sys.executable,
+                "-u",
+                "-m",
+                "gc_monitor",
+                "run",
+                *gc_opts,
+                "-m",
+                str(module_name),
+            ] + list(script_args),
+            # capture_output=True,
+            stdout=subprocess.PIPE,
+            stderr=subprocess.STDOUT,
+            text=True,
+            timeout=30,
+        )
+        return proc
+    except subprocess.TimeoutExpired as exc:
+        print("="*80)
+        print(exc.stdout)
+        print("-"*80)
+        print(exc.stderr)
+        print("="*80)
+
+
 
 
 
