64c61159de6a5dafe4b7a997f7a3a5912e607e5b
Sergey Miryanov <sergey.miryanov@gmail.com>
2026-06-02 22:45:08 +0500

    Remove unecessary import/exports

--- src/gc_monitor/__init__.py
+++ src/gc_monitor/__init__.py
@@ -6,19 +6,16 @@ from .child_process_runner import ChildProcess, ChildProcessRunner
 from .exporters import EventsExporter, JsonlExporter, StdoutExporter, TraceExporter
 from .monitor import EventsMonitor, create_monitor
 from .monitor_thread import MonitorThread
-from .pyperf.hook import GCMonitorHook, gc_monitor_hook
 
 __all__ = [
     "ChildProcess",
     "ChildProcessRunner",
     "EventsExporter",
     "EventsMonitor",
-    "GCMonitorHook",
     "JsonlExporter",
     "MonitorThread",
     "StdoutExporter",
     "TraceExporter",
     "__version__",
     "create_monitor",
-    "gc_monitor_hook",
 ]
